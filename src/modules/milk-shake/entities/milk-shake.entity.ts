e4623bd094d938c319286ae847fbb178655eaf51
Wesley Medeiros <wmedeirosc3@gmail.com>
2024-09-23 10:21:24 -0300

    refactor: update import path for MilkshakeIceCreamFlavor in MilkShake entity

--- src/modules/milk-shake/entities/milk-shake.entity.ts
+++ src/modules/milk-shake/entities/milk-shake.entity.ts
@@ -1,9 +1,9 @@
 import { IceCreamFlavor } from "src/modules/ice-cream-flavor/entities/ice-cream-flavor.entity";
-import { MilkshakeIceCreamFlavor } from "src/modules/ice-cream-flavor/entities/order-ice-cream-flavor.entity";
 import { IceCreamSize } from "src/modules/ice-cream-size/entities/ice-cream-size.entity";
 import { IceCreamSyrup } from "src/modules/ice-cream-syrup/entities/ice-cream-syrup.entity";
 import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { MilkShakeAdditional } from "./milk-shake-additional.entity";
+import { MilkshakeIceCreamFlavor } from "src/modules/ice-cream-flavor/entities/milk-shake-ice-cream-flavor.entity";
 
 @Entity()
 export class MilkShake {
